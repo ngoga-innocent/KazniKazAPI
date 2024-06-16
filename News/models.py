c0ef41827d5816046060b58afaf0c5967d30a204
ngoga-innocent <ngogainnocent1@gmail.com>
2024-06-16 16:45:58 +0200

    migrations

--- News/models.py
+++ News/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 import uuid
+from django.utils import timezone
 # Create your models here.
 class New(models.Model):
     id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     title=models.CharField(max_length=100)
     link=models.TextField()
-    thumbbail=models.ImageField(upload_to='News')
+    thumbnail=models.ImageField(upload_to='News')
+    created_at=models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return self.title
